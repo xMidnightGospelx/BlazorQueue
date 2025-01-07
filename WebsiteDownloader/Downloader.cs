00d2167223f8e721b3b8ff260dd7e5715f982b3a
Geremy Brooks <brooksgeremy@outlook.com>
2025-01-07 12:49:15 -0600

    Add project files.

---
+++ WebsiteDownloader/Downloader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebsiteDownloader
+{
+    internal class Downloader
+    {
+        public Uri Website { get; }
+        public byte[] ResultBytes { get; set; }
+
+        private HttpClient _httpClient;
+        private string _domain;
+
+        public Downloader(Uri website)
+        {
+            Website = website;
+            _httpClient = new HttpClient();
+
+            var matched = Regex.Match(Website.ToString(), @"https?://(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+)");
+            _domain = matched.Groups[2].Value;
+        }
+
+        internal async Task<int> DownloadAsync()
+        {
+            int threadId = Environment.CurrentManagedThreadId;
+            Console.WriteLine($"Before await in GetByteArrayAsync: Thread ID {threadId}");
+
+            ResultBytes = await _httpClient.GetByteArrayAsync(Website);
+            Console.WriteLine($"{Website.ToString()} received. {ResultBytes.Length} bytes.");
+
+            threadId = Environment.CurrentManagedThreadId;
+            Console.WriteLine($"After await in GetByteArrayAsync: Thread ID {threadId}");
+
+            string fileName = $"{_domain}-{Path.GetRandomFileName()[0..5]}.txt";
+            await File.WriteAllBytesAsync(fileName, ResultBytes);
+
+            Console.WriteLine($"{fileName} written to disk.");
+            return ResultBytes.Length;
+        }
+    }
+}
---
+++ WebsiteDownloader/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Concurrent;
+using WebsiteDownloader;
+
+Uri[] sitesUrl = [
+    new Uri("https://www.google.com"),
+    new Uri("https://www.msn.com"),
+    new Uri("https://www.sbc.com"),
+    new Uri("https://www.wikipedia.com"),
+    new Uri("https://www.cnbc.com"),
+    new Uri("https://www.bloomberg.com"),
+    new Uri("https://www.weather.com"),
+];
+
+ConcurrentQueue<Downloader> downloadersQueue = new();
+Parallel.ForEach(sitesUrl, site => downloadersQueue.Enqueue(new Downloader(site)));
+
+//parallel forEach
+
+List<Task<int>> downloadTask = new();
+
+while (downloadersQueue.TryDequeue(out Downloader downloader))
+{ downloadTask.Add(downloader.DownloadAsync()); }
+
+await Task.WhenAll(downloadTask);
+int totalBytes = downloadTask.Sum(t => t.Result);
+Console.WriteLine($"All sites downloaded, {totalBytes} bytes written.");
